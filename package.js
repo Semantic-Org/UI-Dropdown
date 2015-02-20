268db07a47332aba73cfd106b2530e3164f5831d
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:15:37 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,15 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-dropdown',
-  summary : 'Semantic UI - Dropdown (official): Single component release of dropdown',
-  version : '1.9.2',
+  summary : 'Semantic UI - Dropdown: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Dropdown.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'dropdown.css',
+    'dropdown.js'
+  ], 'client');
 });
