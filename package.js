26f89256c48ad4f5c4f5e420f720d64f56181c1a
Pusher Robot <jack@semantic-ui.com>
2015-04-16 12:37:39 -0400

    Updated component to version 1.12.0

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-dropdown',
   summary : 'Semantic UI - Dropdown: Single component release',
-  version : '1.11.7',
+  version : '1.12.0',
   git     : 'git://github.com/Semantic-Org/UI-Dropdown.git',
 });
 
